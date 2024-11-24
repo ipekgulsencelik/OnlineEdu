a764701ef6d9145163e6ff7c88f1a3d2e37ec88d
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2024-11-24 21:18:20 +0300

    Anasayfada iletişim ViewComponent

---
+++ OnlineEdu.WebUI/ViewComponents/UserLayout/_UserLayoutHeaderContactInfoComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI.DTOs.ContactDTOs;
+using OnlineEdu.WebUI.Helpers;
+
+namespace OnlineEdu.WebUI.ViewComponents.UserLayout
+{
+    public class _UserLayoutHeaderContactInfoComponent : ViewComponent
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _client.GetFromJsonAsync<List<ResultContactDTO>>("Contacts");
+            return View(values);
+        }
+    }
+}
