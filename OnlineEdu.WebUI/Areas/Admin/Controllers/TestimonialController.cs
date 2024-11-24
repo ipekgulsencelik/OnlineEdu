f9913dac79e773487188a312aa9e9603764d59eb
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2024-11-24 23:12:13 +0300

    Admin Testimonial Controller

---
+++ OnlineEdu.WebUI/Areas/Admin/Controllers/TestimonialController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI.DTOs.TestimonialDTOs;
+using OnlineEdu.WebUI.Helpers;
+
+namespace OnlineEdu.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("[area]/[controller]/[action]/{id?}")]
+    public class TestimonialController : Controller
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _client.GetFromJsonAsync<List<ResultTestimonialDTO>>("Testimonials");
+            return View(values);
+        }
+
+        public async Task<IActionResult> DeleteTestimonial(int id)
+        {
+            await _client.DeleteAsync($"Testimonials/{id}");
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult CreateTestimonial()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTestimonial(CreateTestimonialDTO createTestimonialDTO)
+        {
+            await _client.PostAsJsonAsync("Testimonials", createTestimonialDTO);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> UpdateTestimonial(int id)
+        {
+            var values = await _client.GetFromJsonAsync<UpdateTestimonialDTO>($"Testimonials/{id}");
+            return View(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateTestimonial(UpdateTestimonialDTO updateTestimonialDTO)
+        {
+            await _client.PutAsJsonAsync("Testimonials", updateTestimonialDTO);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
