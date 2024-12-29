a2f52aba306514bcb83c0fc011f0322940bb3d49
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2024-12-30 02:00:48 +0300

    Son Bloglar ve Abone Olma

--- OnlineEdu.WebUI/Controllers/BlogController.cs
+++ OnlineEdu.WebUI/Controllers/BlogController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI.DTOs.SubscriberDTOs;
+using OnlineEdu.WebUI.Helpers;
 
 namespace OnlineEdu.WebUI.Controllers
 {
     public class BlogController : Controller
     {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Subscribe(CreateSubscriberDTO model)
+        {
+            await _client.PostAsJsonAsync("Subscribers", model);
+            return NoContent();
         }
     }
 }
---
+++ OnlineEdu.WebUI/ViewComponents/Blog/_BlogSubscribeComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineEdu.WebUI.ViewComponents.Blog
+{
+    public class _BlogSubscribeComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
