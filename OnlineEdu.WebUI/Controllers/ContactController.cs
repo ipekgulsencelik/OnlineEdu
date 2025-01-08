e800cccbe7af425ebbe9cbd9427e0772af488c20
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2025-01-08 15:52:10 +0300

    Mesaj

---
+++ OnlineEdu.WebUI/Controllers/ContactController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI.DTOs.MessageDTOs;
+using OnlineEdu.WebUI.Helpers;
+
+namespace OnlineEdu.WebUI.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendMessage(CreateMessageDTO model)
+        {
+            await _client.PostAsJsonAsync("Messages", model);
+            return NoContent();
+        }
+    }
+}
--- OnlineEdu.WebUI/Controllers/UserLayoutController.cs
+++ OnlineEdu.WebUI/Controllers/UserLayoutController.cs
@@ -1,23 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using OnlineEdu.WebUI.DTOs.SubscriberDTOs;
-using OnlineEdu.WebUI.Helpers;
 
 namespace OnlineEdu.WebUI.Controllers
 {
     public class UserLayoutController : Controller
     {
-        private readonly HttpClient _client = HttpClientInstance.CreateClient();
-
         public IActionResult Layout()
         {
             return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Subscribe(CreateSubscriberDTO model)
-        {
-            await _client.PostAsJsonAsync("Subscribers", model);
-            return NoContent();
         }
     }
 }
