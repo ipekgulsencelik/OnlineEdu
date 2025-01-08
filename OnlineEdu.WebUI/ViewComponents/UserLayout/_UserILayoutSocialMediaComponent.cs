047baebc04312e0bf32b8b04eb019bf441a991f9
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2025-01-08 15:51:50 +0300

    Footer

---
+++ OnlineEdu.WebUI/ViewComponents/UserLayout/_UserILayoutSocialMediaComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.WebUI.DTOs.SocialMediaDTOs;
+using OnlineEdu.WebUI.Helpers;
+
+namespace OnlineEdu.WebUI.ViewComponents.UserLayout
+{
+    public class _UserILayoutSocialMediaComponent : ViewComponent
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var socialMedias = await _client.GetFromJsonAsync<List<ResultSocialMediaDTO>>("SocialMedias");
+            return View(socialMedias);
+        }
+    }
+}
